03d6a08bfae6e450a7e97a9aba15675dec3bd683
DanGimeno <dgimeno84@gmail.com>
2025-07-27 13:26:21 +0200

    Add theme switching and color opacity controls

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,13 @@
 import * as React from 'react';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/theme';
+import ThemeRegistry from '@/theme';
 
 export default function RootLayout(props) {
   return (
     <html lang="en">
       <body>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            {props.children}
-          </ThemeProvider>
+          <ThemeRegistry>{props.children}</ThemeRegistry>
         </AppRouterCacheProvider>
       </body>
     </html>
